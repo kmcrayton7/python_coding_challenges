a81bf640d04235ed09846a75dbf64509c76b32d7
Kim Crayton <kmcrayton7@yahoo.com>
2016-02-21 08:27:08 -0500

    Added a version 2: if statement that prints when no bottles are left. Added version 3: that takes in user input to set starting number of bottles.

--- b_o_b.py
+++ b_o_b.py
@@ -1,13 +1,31 @@
-# bottles_of_beer = 99
-# for each_bottle in range(100):
+# version 1
+# bottles_of_beer = 100
+# for each_bottle in range(99):
 # 	bottles_of_beer = bottles_of_beer - 1
-# 	print bottles_of_beer
-# 	bottles_of_beer = bottles_of_beer--
-# 	print "%r Bottles of beer on the wall." % bottles_of_beer
+# 	bottles_left = bottles_of_beer -1
+# 	print "%r bottles of beer on the wall,\n %r bottles of beer.\n Take 1 down, pass it around,\n %r bottles of beer on the wall." % (bottles_of_beer, bottles_of_beer, bottles_left)
 
+# version 2 
+# bottles_of_beer = 100
+# for each_bottle in range(99):
+# 	bottles_of_beer = bottles_of_beer - 1
+# 	bottles_left = bottles_of_beer -1
+# 	print "%r bottles of beer on the wall,\n %r bottles of beer.\n Take 1 down, pass it around,\n %r bottles of beer on the wall." % (bottles_of_beer, bottles_of_beer, bottles_left)
+#         if bottles_left == 0:
+# 		print "No more bottles of beer on the wall"
 
-bottles_of_beer = 100
-for each_bottle in range(99):
-	bottles_of_beer = bottles_of_beer - 1
-	bottles_left = bottles_of_beer -1
-	print "%r bottles of beer on the wall,\n %r bottles of beer.\n Take 1 down, pass it around,\n %r bottles of beer on the wall." % (bottles_of_beer, bottles_of_beer, bottles_left)
+# version 3
+print "How many bottles of beer do you have?"
+
+starting_bottles_of_beer = int(raw_input("> "))
+
+print "%r bottles of beer on the wall,\n %r bottles of beer.\n Take one down,\n pass it around,\n %r bottles of beer on the wall." % (starting_bottles_of_beer, starting_bottles_of_beer, starting_bottles_of_beer - 1)
+print "----------------"
+
+for each_bottle in range(starting_bottles_of_beer):
+	starting_bottles_of_beer = starting_bottles_of_beer - 1
+	if starting_bottles_of_beer == 0:
+		print "No more bottles of beer on the wall."
+	else: 
+	    print "%r bottles of beer on the wall,\n %r bottles of beer.\n Take one down,\n pass it around,\n %r bottles of beer on the wall." % (starting_bottles_of_beer, starting_bottles_of_beer, starting_bottles_of_beer - 1)
+	    print "----------------"
