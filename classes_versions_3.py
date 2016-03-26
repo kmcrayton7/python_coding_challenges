ae9e43a5e4e8f6d434fa791e36a8f5f23d4c7f2b
Kim Crayton <kmcrayton7@yahoo.com>
2016-03-26 12:59:36 -0400

    Added the print statement in version 3 to class

--- classes_versions_3.py
+++ classes_versions_3.py
@@ -1,23 +1,20 @@
-# 1. Put everything in one class
-# 2. Make use of __init__ somewhere and instantiating a property or 2
-# 3. Takes in user input
-# 
-# print "How many bottles of beer do you have?"
-# 
-# starting_bottles_of_beer = int(raw_input("> "))
-# 
-# print "%r bottles of beer on the wall,\n %r bottles of beer.\n Take one down,\n pass it around,\n %r bottles of beer on the wall." % (starting_bottles_of_beer, starting_bottles_of_beer, starting_bottles_of_beer - 1)
-# print "----------------"
-# 
-# for each_bottle in range(starting_bottles_of_beer):
-# 	starting_bottles_of_beer = starting_bottles_of_beer - 1
-# 	if starting_bottles_of_beer == 0:
-# 		print "No more bottles of beer on the wall."
-# 	else: 
-# 	    print "%r bottles of beer on the wall,\n %r bottles of beer.\n Take one down,\n pass it around,\n %r bottles of beer on the wall." % (starting_bottles_of_beer, starting_bottles_of_beer, starting_bottles_of_beer - 1)
-# 	    print "----------------"
+#Original code
+print "How many bottles of beer do you have?"
 
-# Version 1: Put everything in one class
+starting_bottles_of_beer = int(raw_input("> "))
+
+print "%r bottles of beer on the wall,\n %r bottles of beer.\n Take one down,\n pass it around,\n %r bottles of beer on the wall." % (starting_bottles_of_beer, starting_bottles_of_beer, starting_bottles_of_beer - 1)
+print "----------------"
+
+for each_bottle in range(starting_bottles_of_beer):
+	starting_bottles_of_beer = starting_bottles_of_beer - 1
+	if starting_bottles_of_beer == 0:
+		print "No more bottles of beer on the wall."
+	else: 
+	    print "%r bottles of beer on the wall,\n %r bottles of beer.\n Take one down,\n pass it around,\n %r bottles of beer on the wall." % (starting_bottles_of_beer, starting_bottles_of_beer, starting_bottles_of_beer - 1)
+	    print "----------------"
+
+#Version 1: Put everything in one class
 class Bottles(object):
     def starting_bottles_of_beer(self, number):
         print "How many bottles of beer do you have?"
@@ -39,7 +36,7 @@ class Bottles(object):
 twenty = Bottles()
 twenty.starting_bottles_of_beer(20)
 
-# Version 2: Make use of __init__ somewhere and instantiating a property or 2
+#Version 2: Make use of __init__ somewhere and instantiating a property or 2
 class Bottles(object):
     def __init__(self, x):
         self.starting_bottles_of_beer = x
@@ -64,13 +61,16 @@ twenty = Bottles(20)
 twenty.bottles_of_beer()
 
 # Version 3: Takes in user input
-print "How many bottles of beer do you have?"
-starting_bottles_of_beer = int(raw_input("> "))
-        
 class Bottles(object):
+    def __init__(self, starting_bottles_of_beer):
+        self.starting_bottles_of_beer = starting_bottles_of_beer
+ 
+        print "How many bottles of beer do you have?"
+        self.starting_bottles_of_beer = int(raw_input("> "))
+        print "You have %r bottles of beer." % (self.starting_bottles_of_beer)
         
     def bottles_of_beer(self):
-        self.starting_bottles_of_beer = starting_bottles_of_beer
+        
         print (self.starting_bottles_of_beer)
 
         print "%r bottles of beer on the wall,\n %r bottles of beer.\n Take one down,\n pass it around,\n %r bottles of beer on the wall." % (self.starting_bottles_of_beer, self.starting_bottles_of_beer, self.starting_bottles_of_beer - 1)
@@ -84,6 +84,6 @@ class Bottles(object):
 	        else: 
 	            print "%r bottles of beer on the wall,\n %r bottles of beer.\n Take one down,\n pass it around,\n %r bottles of beer on the wall." % (self.each_bottle, self.each_bottle, self.each_bottle - 1)
 	            print "----------------"
-
-twenty = Bottles()
-twenty.bottles_of_beer()
+        
+how_many = Bottles(0)
+how_many.bottles_of_beer()
