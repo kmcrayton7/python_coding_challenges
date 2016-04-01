324ca17656bfe094b6bd667e98fcbda32cbed35d
Kim Crayton <kmcrayton7@yahoo.com>
2016-04-01 17:44:12 -0400

    capitalize_me.py

--- programmr/strings/capitalize_me.py
+++ programmr/strings/capitalize_me.py
@@ -1,8 +1,9 @@
 # Write a program which capitalizes the first letter of a given string.
 
-# sentence = raw_input("Please enter a sentence using all lowercase letters.")
-# sentence = sentence.capitalize()
-# print sentence
+print "Please enter a sentence using all lowercase letters."
+sentence = raw_input('> ')
+sentence = sentence.capitalize()
+print sentence
 
-# I won't focus on it now because I don't understand how to accomplish it from what I've read but I will extend this lesson to capitalize the first letter of each sentence given multiple sentences.
+
 
